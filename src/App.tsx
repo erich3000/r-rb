0b106f5a1548bea58104ebc8953d51ad4b584823
Jens Ihrig <jens.ihrig@mail.de>
2023-08-07 06:39:34 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export const App = () => {
+  return (
+    <div
+      style={{
+        fontSize: "30px",
+        textAlign: "center",
+        marginTop: "50vh"
+      }}
+    >
+      <span style={{ color: "red" }}>RA</span>
+      <span style={{ color: "yellow" }}>IN</span>
+      <span style={{ color: "green" }}>BO</span>
+      <span style={{ color: "blue" }}>W</span>
+    </div>
+  );
+};
